44dc694536c3ef95666b1e65cde2b990adf507c7
Iain S Gillis <iain@iainsgillis.com>
2021-12-01 20:24:51 -0700

    add highlight.php dependency

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInit61b57ed6dfbb65cd70f7b41ac33678ff
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit61b57ed6dfbb65cd70f7b41ac33678ff::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire61b57ed6dfbb65cd70f7b41ac33678ff($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire61b57ed6dfbb65cd70f7b41ac33678ff($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,10 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit61b57ed6dfbb65cd70f7b41ac33678ff
 {
+    public static $files = array (
+        'b6ec61354e97f32c0ae683041c78392a' => __DIR__ . '/..' . '/scrivo/highlight.php/HighlightUtilities/functions.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         'G' => 
         array (
@@ -20,6 +24,20 @@ class ComposerStaticInit61b57ed6dfbb65cd70f7b41ac33678ff
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'H' => 
+        array (
+            'Highlight\\' => 
+            array (
+                0 => __DIR__ . '/..' . '/scrivo/highlight.php',
+            ),
+            'HighlightUtilities\\' => 
+            array (
+                0 => __DIR__ . '/..' . '/scrivo/highlight.php',
+            ),
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'Grav\\Plugin\\HighlightPhpPlugin' => __DIR__ . '/../..' . '/highlight-php.php',
@@ -30,6 +48,7 @@ class ComposerStaticInit61b57ed6dfbb65cd70f7b41ac33678ff
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit61b57ed6dfbb65cd70f7b41ac33678ff::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit61b57ed6dfbb65cd70f7b41ac33678ff::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit61b57ed6dfbb65cd70f7b41ac33678ff::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit61b57ed6dfbb65cd70f7b41ac33678ff::$classMap;
 
         }, null, ClassLoader::class);
