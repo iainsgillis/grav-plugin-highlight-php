5029d82f19f1548e171009703f51064c597e8057
Iain S Gillis <iain@iainsgillis.com>
2022-03-12 17:52:18 -0700

    languages registered in a loop

--- shortcodes/HighlightPhpShortcode.php
+++ shortcodes/HighlightPhpShortcode.php
@@ -9,9 +9,21 @@ class HighlightPhpShortcode extends Shortcode
     public function init()
     {
         $rawHandlers = $this->shortcode->getRawHandlers();
-        $rawHandlers->add('hl', function (ShortcodeInterface $sc) {
-            return "<div>shortcode <span style='font-family: monospace'>hl</span> successfully registered!</div>";
-        });
+        // create an instance of the Highlighter class
+        $hl = new \Highlight\Highlighter();
+
+        // store the result of the listRegisteredLanguages helper method,
+        // passing in the `true` argument to include aliases
+        $langs = array_unique($hl->listRegisteredLanguages(true));
+
+        // loop over the languages...
+        foreach ($langs as $k) {
+
+            // ... and add each one in turn
+            $rawHandlers->add($k, function (ShortcodeInterface $sc) {
+                // TODO: update the logic required by the Thunderer Shortcode engine
+                return "<div>shortcode <span style='font-family: monospace'>" . $sc->getName() . "</span> successfully registered!</div>";
+            });
+        }
     }
 }
-
